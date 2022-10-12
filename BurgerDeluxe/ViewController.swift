b60cbad5dcbd304823d1c4c56744f182734716d8
Kevin Koch <kevin.koch23@d155.org>
2022-10-12 08:11:33 -0500

    initial commit

--- BurgerDeluxe/ViewController.swift
+++ BurgerDeluxe/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var burgerTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //let components = burgerTitle.text!.components(separatedBy: " ")
+        let bT = "\(burgerTitle.text!)"
+        var save = ""
+        var secSave = ""
+        for (index, char) in bT.enumerated(){
+            if index == 55{
+                save = "\(char)"
+            }
+        }
     }
-
-
 }
+
 

d9c521d9c22213b1c78bc340a3ff2fc5858c6e93
Kevin Koch <kevin.koch23@d155.org>
2022-10-21 08:09:48 -0500

    init done, separate class needed

--- BurgerDeluxe/ViewController.swift
+++ BurgerDeluxe/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,7 +17,8 @@ class ViewController: UIViewController {
         @IBOutlet weak var burgerSegment: UISegmentedControl!
         @IBOutlet weak var cookedSegment: UISegmentedControl!
         var burgerList = [""]
-        var cook = 1
+        var type = 1
+        var cook : cookrate!
         var cheese = 0
         var pickles = 0
         var ketchup = 0.0
@@ -30,26 +30,46 @@ class ViewController: UIViewController {
             case mediumrare
             case rare
         }
+        init(cook : cookrate, cheese : Int, pickles : Int, ketchup : Double, buns : Bool){
+            self.cook = cook
+            self.cheese = cheese
+            self.pickles = pickles
+            self.ketchup = ketchup
+            self.buns = buns
+        }
         @IBAction func burgerChoice(_ sender: UISegmentedControl) {
             switch burgerSegment.selectedSegmentIndex{
-            case 0: cheese = 0; pickles = 0; ketchup = 0; buns = true
-            case 1: cheese = 1; pickles = 2; ketchup = 1.5; buns = true
-            case 2: cheese = 5; pickles = 10; ketchup = 20; buns = false
+            case 0: type = 1
+            case 1: type = 2
+            case 2: type = 3
             default:
-                cheese = 0; pickles = 0; ketchup = 0.0; buns = true
+                type = 1
             }
         }
         @IBAction func cookedChoice(_ sender: UISegmentedControl) {
             switch cookedSegment.selectedSegmentIndex{
-            case 0: cook = 1
-            case 1: cook = 2
-            case 2: cook = 3
-            case 3: cook = 4
-            case 4: cook = 5
-            default: cook = 1
+            case 0: cook = .welldone
+            case 1: cook = .mediumwell
+            case 2: cook = .medium
+            case 3: cook = .mediumrare
+            case 4: cook = .rare
+            default: cook = .welldone
             }
         }
         @IBAction func purchase(_ sender: UIButton) {
+            if type == 1{
+                var burger1 = Burger(cook: <#T##cookrate#>, cheese: 0, pickles: 0, ketchup: 1.5, buns: true)
+                burgerList.append("\(burger1)")
+            }
+            else if type == 2{
+                var burger2 = Burger(cook: <#T##cookrate#>, cheese: 2, pickles: 4, ketchup: 3.0, buns: true)
+                burgerList.append("\(burger2)")
+            }
+            else if type == 3{
+                var burger3 = Burger(cook: <#T##cookrate#>, cheese: 40, pickles: 2, ketchup: 42.5, buns: false)
+                burgerList.append("\(burger3)")
+            }
+            print(burgerList)
         }
         
         
